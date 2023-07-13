b9125d35712cb4f9b4548af385685d9b2d922a68
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-13 12:57:40 +0300

    modified:   7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,14 @@
 -- Updates the users table by computing the average from corrections
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser  (IN user_id INT)
        BEGIN
-            DECLARE users_id INT DEFAULT 0;
             DECLARE avg FLOAT DEFAULT 0;
-            SELECT id INTO users_id FROM projects WHERE id=user_id;
-            IF users_id > 0 THEN
-                -- USER EXISTS:- capture their project_id next
-                select sum(score)/count(*) INTO avg from corrections where user_id=users_id group by user_id;
-                UPDATE users set average_score=avg where user_id=users_id;
-            END IF;
+
+            -- USER EXISTS:- capture their project_id next
+            SET avg = (select sum(score)/count(*) from corrections AS c where c.user_id=user_id group by c.user_id);
+            UPDATE users set average_score=avg where id=user_id;
 
         END$$
 DELIMITER ;
