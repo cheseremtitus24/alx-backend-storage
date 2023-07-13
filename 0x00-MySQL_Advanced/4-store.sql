ce34c4dfa375f9c2b3f9b06f36647fa43708b122
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-13 08:09:29 +0300

    new file:   4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,17 @@
+-- creates a trigger that update the quantity of items left
+-- in store after a successful order placement.
+-- it allows the items quantity in stock to go below 0.
+
+DELIMITER $$
+USE `holberton` $$
+CREATE 
+    DEFINER=`root`@`127.0.0.1` 
+    TRIGGER `holberton`.`orders_AINS` 
+        AFTER INSERT ON `holberton`.`orders`
+            FOR EACH ROW
+-- Edit trigger body code below this line. Do not edit lines above this one BEGIN 
+BEGIN
+                UPDATE items set quantity=quantity - NEW.number WHERE name=New.item_name;
+END$$
+DELIMITER ;
+-- Create Trigger `holberton`.`orders_AINS` AFTER INSERT ON `holberton`.`orders` FOR EACH ROW UPDATE items SET quantity=quantity - NEW.number where name=New.item_name;
