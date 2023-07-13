4d6652966291107b939f0cd2d58b9dd98a3918fb
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-13 13:02:29 +0300

    Refactoring: Eliminated the select ... into var_name -- for IN vars rather used SET varname = (select sql statement);

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,10 +8,10 @@ CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN scor
        BEGIN
             DECLARE proj_id INT DEFAULT 0;
             DECLARE users_id INT DEFAULT 0;
-            SELECT id INTO users_id FROM projects WHERE id=user_id;
+            SET users_id = (SELECT id FROM projects WHERE id=user_id);
             IF users_id > 0 THEN
                 -- USER EXISTS:- capture their project_id next
-                SELECT id INTO proj_id FROM projects WHERE name=project_name;
+                SET proj_id = (SELECT id FROM projects WHERE name=project_name);
                 IF proj_id > 1 THEN
                     -- PROJECT NAME EXISTS
                     INSERT INTO corrections(user_id,project_id,score) VALUES(user_id,proj_id,score);
@@ -19,7 +19,7 @@ CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN scor
                     -- PROJECT NAME DOES NOT EXIST THEREFORE CREATE IT
                     INSERT INTO projects(name) VALUES (project_name);
                     -- RETURN LAST INSERT ROW ID AUTO_INCREMENT HAS BEEN SET HERE
-                    SELECT LAST_INSERT_ID() INTO proj_id;
+                    SET proj_id = (SELECT LAST_INSERT_ID());
                     INSERT INTO corrections(user_id,project_id,score) VALUES(user_id,proj_id,score);
 
                 END IF;
