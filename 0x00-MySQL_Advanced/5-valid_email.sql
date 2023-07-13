b9b5cac6c3605a782d7afd85c59c96a1fae8ab86
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-13 08:58:25 +0300

    Feat Upd: Handled Case when valid email is 1 and has to be updated to zero

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -9,9 +9,10 @@ CREATE
             FOR EACH ROW
 -- Edit trigger body code below this line. Do not edit lines above this one BEGIN 
 BEGIN
-           IF NEW.email != OLD.email THEN
+           IF NEW.email != OLD.email and OLD.valid_email = 1 THEN
                 SET NEW.valid_email=0; 
             ENDIF;
 
 END$$
 DELIMITER ;
+-- Create Trigger `holberton`.`orders_AINS` AFTER INSERT ON `holberton`.`orders` FOR EACH ROW UPDATE items SET quantity=quantity - NEW.number where name=New.item_name;
