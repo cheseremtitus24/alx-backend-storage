9d1da7afee5f1f52d3ad8e18a2a32a37ef934a62
Cheserem Titus <cheseremtitus24@gmail.com>
2023-07-19 20:17:41 +0300

    Revert "	modified:   exercise.py"
    This reverts commit 4ef7e6a545f2a4a17222af4f1fa56cfb4a4d622e.
    
    	0x02-redis_basic/exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 This module models a Caching System that stores the runtime values stored data
 """
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 import redis
 
@@ -32,3 +32,46 @@ class Cache:
         rand_key_string: str = str(randkey)
         self._redis.set(name=rand_key_string, value=data)
         return rand_key_string
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """Retrieve Data from Redis DB"""
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """
+        Returns a decoded string
+        :param key: Redis dictionary Unique access key
+        :type key: string
+        :return: a decoded string
+        :rtype: string
+        """
+        val = self._redis.get(key)
+        return val.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+        Get Integer
+        :param key: dictionary key
+        :type key: str
+        :return: decoded integer value
+        :rtype: int
+        """
+        val = self._redis.get(key)
+        try:
+            val = int(val.decode("utf-8"))
+        except Exception:
+            val = 0
+        return val
+
+    def __del__(self) -> None:
+        """
+        Destroys the redis connection when object
+        goes out of scope
+        :return: None
+        :rtype: None
+        """
+        self._redis.close()
