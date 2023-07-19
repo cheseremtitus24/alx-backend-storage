d222063ac1477d4df1202cccf8a0a76736ffdc80
Cheserem Titus <cheseremtitus24@gmail.com>
2023-07-19 18:40:37 +0300

    Doc: Added Documentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+
 """
 import uuid
 from typing import Union
@@ -8,6 +9,9 @@ import redis
 
 
 class Cache:
+    """
+    Saves data to Redis DB
+    """
     _redis: redis.Redis = redis.Redis()
 
     def __init__(self):
@@ -15,10 +19,20 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        :param data: is saved as a value in the redis DB
+        :type data: str | bytes | int | float
+        :return: a unique dictionary key
+        :rtype: str
+        """
         randkey: uuid.UUID = uuid.uuid4()
         rand_key_string: str = str(randkey)
         self._redis.set(name=rand_key_string, value=data)
         return rand_key_string
 
-    def __del__(self):
+    def __del__(self) -> None:
+        """
+        :return: None
+        :rtype: None
+        """
         self._redis.close()
